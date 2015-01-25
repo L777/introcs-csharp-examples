adf3e9b793825447dd9a7339901739a71f5d77a0
L777 <lp8678@gmail.com>
2015-01-24 22:55:21 -0600

    created files/discovered a path

--- grade_file_homework_stub/grade_files.cs
+++ grade_file_homework_stub/grade_files.cs
@@ -9,23 +9,41 @@ namespace IntroCS
       /// on the command line, or provided interactively.
       public static void Main(string[] args)
       {
-         //18.7.3:  - prompt course abreviation from the user
+                 //18.7.3:  - prompt course abreviation from the user
          Console.WriteLine ("Enter course abreviation(e.g.Comp150,Comp170 etc): ");
-         string abreviation=Console.ReadLine();
+         string abreviation = Console.ReadLine ();
 
-                // creates the two master files name as a string values // we will use these values during files creation
-         string categories_name=("categories_"+abreviation+".txt");
-         string students_name=("students_"+abreviation+".txt");
-     //    Console.WriteLine (categories_name + "\n " + students_name);
+         // creates the two master files name as a string values // we will use these values during files creation
+         string categories_name = ("categories_" + abreviation + ".txt");
+         string students_name = ("students_" + abreviation + ".txt");
+
+         //    Console.WriteLine (categories_name + "\n " + students_name);
+
+         StreamWriter writer = new StreamWriter (categories_name);
+         StreamWriter writer2 = new StreamWriter (students_name);
+
+         writer.Close ();
+         writer2.Close ();
+
+         string pathToCatFile = GetPath (categories_name);
+         string pathToStudentFile = GetPath (students_name);
+
+         Console.WriteLine (pathToCatFile);
+         Console.WriteLine (pathToStudentFile);
 
 
-         /*
+
+
+      }
+
 
          /// Find a directory containing the filename
          /// and return the full file path, if it exists. 
          /// Otherwise return null.
          public static string GetPath(string filename)
          {
+          string[] paths = { ".", "..", Path.Combine("..", "..") };
+
             foreach (string dir in paths) {
                string filePath = Path.Combine(dir, filename);
                if (File.Exists(filePath))
@@ -36,10 +54,9 @@ namespace IntroCS
 
 
          // TEMPORARY line to get oriented to the file system:
-         Console.WriteLine("Current directory: " +
-                           Directory.GetCurrentDirectory());
+        // Console.WriteLine("Current directory: " +Directory.GetCurrentDirectory());
       }
-                                           // codeIndex chunk                                          
+   /*                                        // codeIndex chunk                                          
       /// Take the first letter code for a catagory, and 
       /// return the index of that category in categories.
       static int codeIndex(string code, string[] categories)
@@ -53,6 +70,5 @@ namespace IntroCS
       }
    }  
    // end codeIndex chunk
-   */
+*/
 }
-}}
